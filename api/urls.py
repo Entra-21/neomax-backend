fc97ec525fa8ad49dca51e50e7c56039f6c6cb8e
Nill Junior <89917303+nilljr@users.noreply.github.com>
2024-02-04 12:30:41 -0300

    Create project root folders and initial setup (#1)

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path, include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"", views.)
+router.register(r"", views.)
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+]
